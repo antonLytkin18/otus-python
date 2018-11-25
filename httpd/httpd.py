92e2da6db4b81c08a4383726a5966df90ed64766
Anton Lytkin <a.lytkin@worksolutions.ru>
2018-11-25 14:18:48 +0300

    Lesson 5. Http Server

---
+++ httpd/httpd.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import argparse
+import logging
+import os
+import socket
+from multiprocessing import Process
+
+from lib import Request, Response
+
+
+class Server:
+
+    CHUNK_SIZE = 1024
+    HOST = ''
+    PORT = 8080
+
+    def __init__(self, root, port):
+        self._document_root = root
+        if self.is_document_root_invalid():
+            raise Exception('Invalid document root')
+        self._socket = socket.socket()
+        self._socket.bind((self.HOST, port))
+        self._socket.listen()
+
+    def is_document_root_invalid(self):
+        return self._document_root and not os.path.isdir(self._document_root)
+
+    def create_request(self, client_socket):
+        request_bytes = b''
+        expected_size = 0
+        request = None
+        while len(request_bytes) >= expected_size:
+            chunk_bytes = client_socket.recv(self.CHUNK_SIZE)
+            request_bytes += chunk_bytes
+            request = Request(request_bytes)
+            if request.is_complete():
+                break
+            expected_size += self.CHUNK_SIZE
+        return request
+
+    def serve(self):
+        client_socket, address_info = self._socket.accept()
+        with client_socket:
+            request = self.create_request(client_socket)
+            response = Response(request, self._document_root)
+            client_socket.sendall(response.to_bytes())
+
+    def serve_forever(self):
+        while True:
+            self.serve()
+
+
+if __name__ == "__main__":
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format='[%(asctime)s] %(levelname).1s %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-w', '--workers', help='workers count', default=1, type=int)
+    parser.add_argument('-r', '--document_root', help='document root path', default='', type=str)
+    parser.add_argument('-p', '--port', help='port', default=Server.PORT, type=int)
+    args = parser.parse_args()
+    workers_count = args.workers if args.workers > 0 else 1
+    try:
+        server = Server(args.document_root, args.port)
+    except Exception as e:
+        logging.error(e)
+        exit()
+    workers = []
+    worker = Process(target=server.serve_forever)
+
+    for _ in range(workers_count):
+        worker = Process(target=server.serve_forever)
+        worker.start()
+        workers.append(worker)
+    try:
+        for worker in workers:
+            worker.join()
+    except KeyboardInterrupt:
+        for worker in workers:
+            worker.terminate()
+            logging.info(f'Worker {worker.pid} was terminated')
+
---
+++ httpd/lib.py
@@ -0,0 +1,141 @@
+import mimetypes
+import os
+from datetime import datetime
+from time import mktime
+from urllib import parse
+from wsgiref.handlers import format_date_time
+
+CLRF = '\r\n'
+ENCODING = 'utf-8'
+
+
+class Request:
+
+    _method = None
+    _query_string = ''
+    _query_params = ''
+    _http = ''
+    _path = '/'
+    _headers = []
+
+    def __init__(self, request_bytes):
+        self._request_data = request_bytes.decode(ENCODING)
+        if not self.is_complete():
+            return
+        request_string, headers_string = self._request_data.split(CLRF, 1)
+        self._method, self._query_string, self._http = request_string.split(' ')
+        params = self._query_string.split('?', 1)
+        self._path = parse.unquote(params[0])
+        self._query_params = params[1] if len(params) > 1 else ''
+        self._headers = headers_string.splitlines()
+
+    def get_request_data(self):
+        return self._request_data
+
+    def is_complete(self):
+        return self._request_data.endswith(CLRF * 2)
+
+    def get_query_string(self):
+        return self._query_string
+
+    def get_path(self):
+        return self._path
+
+    def get_method(self):
+        return self._method
+
+
+class Response:
+
+    OK = 200
+    BAD_REQUEST = 400
+    NOT_FOUND = 404
+    NOT_ALLOWED = 405
+    STATUS_REASON_PHRASES = {
+        OK: 'OK',
+        BAD_REQUEST: 'Bad Request',
+        NOT_FOUND: 'Not Found',
+        NOT_ALLOWED: 'Not allowed',
+    }
+
+    _allowed_methods = ['GET', 'HEAD']
+    _status = OK
+    _body = b''
+    _content_type = 'text/html'
+    _content_length = 0
+
+    def __init__(self, request, document_root):
+        self._request = request
+        if not self.has_request_data():
+            self.set_bad_request()
+            return
+        if not self.is_method_allowed():
+            self.set_not_allowed()
+            return
+        self._file_path = self.build_file_path(document_root)
+        if not self.is_file_exists():
+            self.set_not_found()
+            return
+        self._content_type = self.get_content_type()
+        self._body = self.get_content()
+        self._content_length = len(self._body)
+        if self.is_head_request():
+            self.clear_body()
+
+    def is_method_allowed(self):
+        return self._request.get_method() in self._allowed_methods
+
+    def set_not_allowed(self):
+        self._status = self.NOT_ALLOWED
+        return self
+
+    def set_not_found(self):
+        self._status = self.NOT_FOUND
+        return self
+
+    def set_bad_request(self):
+        self._status = self.BAD_REQUEST
+        return self
+
+    def build_file_path(self, document_root):
+        path = self._request.get_path()
+        path += 'index.html' if path.endswith('/') else ''
+        root = document_root if document_root else os.path.dirname(os.path.abspath(__file__))
+        return root + os.path.normpath(path)
+
+    def get_content(self):
+        with open(self._file_path, 'rb') as file:
+            return file.read()
+
+    def is_file_exists(self):
+        return os.path.exists(self._file_path)
+
+    def get_content_type(self):
+        content_type, encoding = mimetypes.guess_type(self._file_path)
+        return content_type
+
+    def has_request_data(self):
+        return self._request.get_request_data()
+
+    def clear_body(self):
+        self._body = b''
+        return self
+
+    def is_head_request(self):
+        return self._request.get_method() == 'HEAD'
+
+    def get_headers(self):
+        return [
+            f'HTTP/1.1 {self._status} {self.get_reason_phrase()}',
+            f'Date: {format(format_date_time(mktime(datetime.now().timetuple())))}',
+            f'Content-Type: {self._content_type}',
+            f'Content-Length: {self._content_length}',
+            'Server: Poor man\'s server',
+            'Connection: close',
+        ]
+
+    def get_reason_phrase(self):
+        return self.STATUS_REASON_PHRASES[self._status]
+
+    def to_bytes(self):
+        return (CLRF.join(self.get_headers()) + CLRF * 2).encode(ENCODING) + self._body
